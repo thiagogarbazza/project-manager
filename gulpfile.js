9bdfbe58e3963fabe0fbddd49b13c064cc816c3e
thiagogarbazza <thiagogarbazza@gmail.com>
2016-08-23 14:44:26 -0300

    add gulp-task build-js

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,5 @@ const runSequence = require('run-sequence');
 const tasks = require('require-dir')('./src/tasks');
 
 gulp.task('build', cb => {
-  return runSequence('build-clean', ['build-css'], cb);
+  return runSequence('build-clean', 'build-copy', ['build-css', 'build-js'], cb);
 });

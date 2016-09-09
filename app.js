5ffc1b6100322d2d9bab58286f3415e09c5c3833
thiagogarbazza <thiagogarbazza@gmail.com>
2016-09-09 10:11:07 -0300

    remove test in app.js

--- app.js
+++ app.js
@@ -2,27 +2,19 @@ const path = require('path');
 const glob = require('glob');
 const yaml = require('yamljs');
 
-const dbFiles = path.join(__dirname, 'src/api-db/**/*.yml');
-glob.sync(dbFiles, {}).forEach((dbFile, index) => {
-  console.log(index, dbFile);
-  let db = yaml.load(dbFile);
-  console.error('first data', db.load[0].data[0]);
-});
+const consign = require("consign");
+const express = require("express");
 
+const app = express();
 
-// const consign = require("consign");
-// const express = require("express");
+consign({verbose: false})
+  .include("src/configuration.js")
+  .then("src/startup/start-sequelize.js")
+  .then("src/startup/loading-models.js")
+  .then("src/startup/authentication.js")
+  .then("src/startup/middlewares.js")
+  .then("src/startup/loading-routes.js")
+  .then("src/startup/boot.js")
+  .into(app);
 
-// const app = express();
-
-// consign({verbose: false})
-//   .include("src/configuration.js")
-//   .then("src/startup/start-sequelize.js")
-//   .then("src/startup/loading-models.js")
-//   .then("src/startup/authentication.js")
-//   .then("src/startup/middlewares.js")
-//   .then("src/startup/loading-routes.js")
-//   .then("src/startup/boot.js")
-//   .into(app);
-
-// module.exports = app;
+module.exports = app;

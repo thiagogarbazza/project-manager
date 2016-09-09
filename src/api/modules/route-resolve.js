4a9c8be62ec5800b7b10eb92f05eab071ee528e9
thiagogarbazza <thiagogarbazza@gmail.com>
2016-09-09 14:55:32 -0300

    create models/route-resolve

--- src/api/modules/route-resolve.js
+++ src/api/modules/route-resolve.js
@@ -1,19 +1,40 @@
 'use strict';
 const HttpStatus = require('http-status-codes');
 
-
-
-
-
 module.exports = {
-  get
+  create,
+  destroy,
+  find,
+  update
 };
 
-function get(response, promise) {
+function create(response, promise) {
+  promise
+    .then(result => response.status(HttpStatus.CREATED).json(result))
+    .catch(error => resolveCatch(error, response));
+}
+
+function destroy(response, promise) {
+  promise
+    .then(() => response.sendStatus(HttpStatus.NO_CONTENT))
+    .catch(error => resolveCatch(error, response));
+}
+
+function find(response, promise) {
   promise
     .then(result => response.json(result))
-    .catch(error => response.status(HttpStatus.PRECONDITION_FAILED).json({
-      error: error.errors,
-      message: error.message
-    }));
+    .catch(error => resolveCatch(error, response));
+}
+
+function update(response, promise) {
+  promise
+    .then(() => response.sendStatus(HttpStatus.NO_CONTENT))
+    .catch(error => resolveCatch(error, response));
+}
+
+function resolveCatch(error, response) {
+  return response.status(HttpStatus.PRECONDITION_FAILED).json({
+    error: error.errors,
+    message: error.message
+  });
 }

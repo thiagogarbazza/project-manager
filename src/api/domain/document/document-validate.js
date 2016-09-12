0938062080396c9e10a595815bbb8e007ea05330
thiagogarbazza <thiagogarbazza@gmail.com>
2016-09-12 16:54:38 -0300

    update business error

--- src/api/domain/document/document-validate.js
+++ src/api/domain/document/document-validate.js
@@ -1,11 +1,11 @@
 'use strict';
-const BusinessMessage = require('business-error').Message;
+const BusinessCase = require('business-error').Case;
 const BusinessError = require('business-error').Error;
 
 class DocumentValidate {
   constructor(app) {
     this.app = app;
-    this.errors = [];
+    this.businessCases = [];
   }
 
   validateOnCreate(document) {
@@ -15,22 +15,22 @@ class DocumentValidate {
   }
 
   processValidate() {
-    if (this.errors.length !== 0) {
-      throw new BusinessError(this.errors);
+    if (this.businessCases.length !== 0) {
+      throw new BusinessError(this.businessCases);
     }
   }
 
   codeIsRequire(document) {
     if (!document.code) {
-      const error = new BusinessMessage('document.code', 'The code is require.');
-      this.errors.push(error);
+      const businessCase = new BusinessCase('document.code', 'The code is require.');
+      this.businessCases.push(businessCase);
     }
   }
 
   nameIsRequire(document) {
     if (!document.name) {
-      const error = new BusinessMessage('document.name', 'The name is required.');
-      this.errors.push(error);
+      const businessCase = new BusinessCase('document.name', 'The name is required.');
+      this.businessCases.push(businessCase);
     }
   }
 }

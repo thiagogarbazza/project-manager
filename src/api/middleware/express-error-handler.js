7de4d08d44a84b50ba1dfec57ff2de5ca32c86a0
thiagogarbazza <thiagogarbazza@gmail.com>
2016-10-26 16:36:05 -0200

    create meddleware express-error-handler.js

---
+++ src/api/middleware/express-error-handler.js
@@ -0,0 +1,10 @@
+'use strict';
+const routeResolver = require('express-route-resolver');
+
+module.exports = app => {
+  app.use(BusinessErrorHandler);
+};
+
+function BusinessErrorHandler(err, req, res, next) {
+  routeResolver.onError(res, err);
+}

90b52bcc11e34f2b7c745c7a07509f78cd883334
thiagogarbazza <thiagogarbazza@gmail.com>
2016-10-26 16:52:11 -0200

    remove old middleware express-route-cors

--- src/api/startup/start-cors.js
+++
@@ -1,9 +0,0 @@
-const cors = require('cors');
-
-module.exports = app => {
-  app.use(cors({
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    origin: ['http://localhost:3000']
-  }));
-};

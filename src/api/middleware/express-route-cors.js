a52e02e7cef3248268840b63cc53cf56b544b8e9
thiagogarbazza <thiagogarbazza@gmail.com>
2016-10-26 16:39:41 -0200

    create middleware express-route-cors.js

---
+++ src/api/middleware/express-route-cors.js
@@ -0,0 +1,10 @@
+'use strict';
+const cors = require('cors');
+
+module.exports = app => {
+  app.use(cors({
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: ['http://localhost:3000']
+  }));
+};

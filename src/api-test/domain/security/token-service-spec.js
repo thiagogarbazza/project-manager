6197e0b1c8b2e838638866ed3d91b1259a566162
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-01 18:19:46 -0200

    create token service

---
+++ src/api-test/domain/security/token-service-spec.js
@@ -0,0 +1,98 @@
+'use strict';
+
+describe('api domain security token service', () => {
+  const APP = {};
+  let TokenService;
+  let tokenService;
+  let USER;
+
+  before(() => {
+    const USER_SERVICE = class {
+      constructor(app) {}
+      findByEmail(email) {
+        return email === 'thiagogarbazza@gmail.com' ? Promise.resolve(USER) : Promise.reject({
+          name: 'Error'
+        });
+      }
+    }
+    mockery.registerMock('./user/user-service', USER_SERVICE);
+    mockery.registerAllowable('../../../api/domain/security/token-service');
+    TokenService = require('../../../api/domain/security/token-service');
+  });
+
+  after(() => {
+    mockery.deregisterMock('./user-service');
+  });
+
+
+  beforeEach(() => {
+    dottie.set(APP, 'configuration.authentication.passphrase', 'TESTE');
+
+    USER = {
+      email: 'thiagogarbazza@gmail.com',
+      id: '36726e94-fbd2-4152-a0ac-92ac494ce65a',
+      name: 'Thiago Garbazza',
+      password: 'swordfish'
+    };
+
+    tokenService = new TokenService(APP);
+  });
+
+  it('should be defined', () => {
+    expect(tokenService).to.not.be.undefined;
+  });
+
+  describe('# generate', () => {
+    it('try to generate token without informing the email and password', done => {
+      tokenService.generate({})
+        .then(() => done('should not generate TOKEN, since they were not sent e-mail and password'))
+        .catch(error => {
+          expect(error.name).to.equal('Error');
+          done();
+        });
+    });
+
+    it('try to generate token informing the password and not informing the email', done => {
+      tokenService.generate({email: 'thiagogarbazza@gmail.com'})
+        .then(() => done('should not generate TOKEN, since they were not sent password'))
+        .catch(error => {
+          expect(error.name).to.equal('Error');
+          done();
+        });
+    });
+
+    it('try to generate token informing the password and not informing the email', done => {
+      tokenService.generate({email: 'thiagogarbazza@gmail.com'})
+        .then(() => done('should not generate TOKEN, because the data is invalid'))
+        .catch(error => {
+          expect(error.name).to.equal('Error');
+          done();
+        });
+    });
+
+    it('try to generate token informing valid email and password', done => {
+      USER.isPassword = simpleMock.stub().returnWith(true);
+
+      tokenService.generate({email: 'thiagogarbazza@gmail.com',password: 'swordfish'})
+        .then(result => {
+          expect(USER.isPassword.callCount).to.equal(1);
+          expect(result.name).to.equal('Thiago Garbazza');
+          expect(result.token).to.not.be.undefined;
+          done();
+        })
+        .catch(done);
+    });
+
+    it('try to generate token informing invalid email and password', done => {
+      USER.isPassword = simpleMock.stub().returnWith(false);
+
+      tokenService.generate({email: 'thiagogarbazza@gmail.com',password: 'swordffish'})
+        .then(() => done('should not generate TOKEN, because the data is invalid'))
+        .catch(error => {
+          expect(USER.isPassword.callCount).to.equal(1);
+          expect(error.name).to.equal('Error');
+          done();
+        });
+    });
+  });
+});
---
+++ src/api/domain/security/token-service.js
@@ -0,0 +1,42 @@
+'use strict';
+const UserService = require('./user/user-service');
+const jwt = require('jwt-simple');
+
+class TokenService {
+  constructor(app) {
+    this.configuration = app.configuration.authentication;
+    this.userService = new UserService(app);
+  }
+
+  generate({email, password}) {
+    if (!email || !password) {
+      return Promise.reject(new Error('To generate token a email and password should be informed'));
+    }
+
+    return this.userService.findByEmail(email)
+      .then(user => {
+        console.error(user);
+
+        if (user && user.isPassword(password)) {
+          const tokenCreateAt = new Date();
+          const payload = {
+            id: user.id,
+            tokenCreateAt
+          };
+
+          const response = {
+            email: user.email,
+            id: user.id,
+            name: user.name,
+            token: jwt.encode(payload, this.configuration.passphrase),
+            tokenCreateAt
+          };
+
+          return Promise.resolve(response);
+        }
+        return Promise.reject(new Error('Email and password were not recognized'));
+      });
+  }
+}
+
+module.exports = TokenService;

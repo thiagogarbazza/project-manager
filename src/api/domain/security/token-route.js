c88c25862c953658b18e2a92203349ccd2cf927d
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-01 18:23:27 -0200

    create token route

---
+++ src/api/domain/security/token-route.js
@@ -0,0 +1,16 @@
+'use strict';
+const TokenService = require('./token-service');
+const HttpStatus = require('http-status-codes');
+
+module.exports = app => {
+  const tokenService = new TokenService(app);
+
+  app.route('/service/token')
+    .post((req, res) => {
+      tokenService.generate(req.body)
+        .then(result => res.json(result))
+        .catch(error => res.status(HttpStatus.UNAUTHORIZED).json({
+          message: error.message
+        }));
+    });
+};

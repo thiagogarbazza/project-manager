1f29ea8676e85082745d4b5de6f17daef14d0895
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-01 14:26:24 -0200

    criando user-validate

---
+++ src/api-test/domain/security/user/user-validate-spec.js
@@ -0,0 +1,161 @@
+'use strict';
+const UserValidate = require('../../../../api/domain/security/user/user-validate');
+
+describe('api domain security user validate', () => {
+  const APP = {};
+  let USER;
+  let userValidate;
+
+  beforeEach(() => {
+    dottie.set(APP, 'domain.security.user.UserModel', {});
+
+    USER = {
+      email: 'thiagogarbazza@gmail.com',
+      id: '36726e94-fbd2-4152-a0ac-92ac494ce65a',
+      nome: 'Thiago Garbazza'
+    };
+
+    userValidate = new UserValidate(APP);
+  });
+
+  it('should be defined', () => {
+    expect(userValidate).to.not.be.undefined;
+  });
+
+  it('E-mail should be required', done => {
+    delete USER.email;
+
+    userValidate.emailIsRequired(USER)
+      .then(result => {
+        expect(result.code).to.equal('user.email.required');
+        expect(result.message).to.equal('E-mail is required');
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('E-mail should be valid', done => {
+    USER.email = 'thiagogarbazza#gmail.com';
+
+    userValidate.emailIsValid(USER)
+      .then(result => {
+        expect(result.code).to.equal('user.email.valid');
+        expect(result.message).to.equal('E-mail must be valid');
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('E-mail should be maximum 250 characters', done => {
+    USER.email = properties.BIG_TEXT;
+
+    userValidate.emailMustHaveMaximum250Characters(USER)
+      .then(result => {
+        expect(result.code).to.equal('user.email.maxlength');
+        expect(result.message).to.equal('E-mail must have a maximum of 250 characters');
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('E-mail should be unique, a new and unique case', done => {
+    const anotherUser = clone(USER);
+    anotherUser.email = 'fake@gmail.com';
+    APP.domain.security.user.UserModel.findOne = simpleMock.stub().resolveWith();
+
+    userValidate.emailMustBeUnique(anotherUser)
+      .then(result => {
+        expect(APP.domain.security.user.UserModel.findOne.callCount).to.equal(1);
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('E-mail should be unique, a new and non unique case', done => {
+    const anotherUser = clone(USER);
+    delete anotherUser.id;
+    APP.domain.security.user.UserModel.findOne = simpleMock.stub().resolveWith(USER);
+
+    userValidate.emailMustBeUnique(anotherUser)
+      .then(result => {
+        expect(result.code).to.equal('user.email.unique');
+        expect(result.message).to.equal('E-mail must be unique');
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('E-mail should be unique, a update and unique case', done => {
+    const anotherUser = clone(USER);
+    APP.domain.security.user.UserModel.findOne = simpleMock.stub().resolveWith(USER);
+
+    userValidate.emailMustBeUnique(anotherUser)
+      .then(result => {
+        expect(APP.domain.security.user.UserModel.findOne.callCount).to.equal(1);
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('E-mail should be unique, a update and non unique case', done => {
+    const anotherUser = clone(USER);
+    anotherUser.id = 'e218343f-8f9e-4b2b-bbf6-e862506b34da';
+    APP.domain.security.user.UserModel.findOne = simpleMock.stub().resolveWith(USER);
+
+    userValidate.emailMustBeUnique(anotherUser)
+      .then(result => {
+        expect(result.code).to.equal('user.email.unique');
+        expect(result.message).to.equal('E-mail must be unique');
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('Name should be required', done => {
+    delete USER.name;
+
+    userValidate.nameIsRequired(USER)
+      .then(result => {
+        expect(result.code).to.equal('user.name.required');
+        expect(result.message).to.equal('Name is required');
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('Name should be maximum 100 characters', done => {
+    USER.name = properties.BIG_TEXT;
+
+    userValidate.nameMustHaveMaximum100Characters(USER)
+      .then(result => {
+        expect(result.code).to.equal('user.name.maxlength');
+        expect(result.message).to.equal('Name must have a maximum of 100 characters');
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('Password should be required', done => {
+    delete USER.password;
+
+    userValidate.passwordIsRequired(USER)
+      .then(result => {
+        expect(result.code).to.equal('user.password.required');
+        expect(result.message).to.equal('Password is required');
+        return done();
+      })
+      .catch(done);
+  });
+
+  it('Password should be minimum 100 characters', done => {
+    USER.password = ')!@#';
+
+    userValidate.passwordMustHaveMinimum5Characters(USER)
+      .then(result => {
+        expect(result.code).to.equal('user.password.minlength');
+        expect(result.message).to.equal('Password must have a minimum  of 5 characters');
+        return done();
+      })
+      .catch(done);
+  });
+});
---
+++ src/api/domain/security/user/user-validate.js
@@ -0,0 +1,115 @@
+'use strict';
+const {AbstractValidate, BusinessCase} = require('business-error');
+const {trim} = require('lodash');
+const {isEmail} = require('validator');
+
+const EMAIL_MAXLENGTH = 250;
+const NAME_MAXLENGTH = 100;
+const PASSWORD_MINLENGTH = 5;
+
+class UserValidate extends AbstractValidate {
+  constructor(app) {
+    super();
+    this.userModel = app.domain.security.user.UserModel;
+  }
+
+  onCreate(user) {
+    return this.resolveValidationPromises(
+      this.emailIsRequired(user),
+      this.emailIsValid(user),
+      this.emailMustHaveMaximum250Characters(user),
+      this.emailMustBeUnique(user),
+      this.nameIsRequired(user),
+      this.nameMustHaveMaximum100Characters(user),
+      this.passwordIsRequired(user),
+      this.passwordMustHaveMinimum5Characters(user)
+    );
+  }
+
+  onUpdate(user) {
+    return this.resolveValidationPromises(
+      this.emailIsRequired(user),
+      this.emailIsValid(user),
+      this.emailMustHaveMaximum250Characters(user),
+      this.emailMustBeUnique(user),
+      this.nameIsRequired(user),
+      this.nameMustHaveMaximum100Characters(user),
+      this.passwordIsRequired(user),
+      this.passwordMustHaveMinimum5Characters(user)
+    );
+  }
+
+  emailIsRequired({email}) {
+    if (!trim(email)) {
+      const businessCase = new BusinessCase('user.email.required', 'E-mail is required');
+      return Promise.resolve(businessCase);
+    }
+    return Promise.resolve();
+  }
+
+  emailIsValid({email}) {
+    if (email && !isEmail(email)) {
+      const businessCase = new BusinessCase('user.email.valid', 'E-mail must be valid');
+      return Promise.resolve(businessCase);
+    }
+    return Promise.resolve();
+  }
+
+  emailMustHaveMaximum250Characters({email}) {
+    if (email && email.length > EMAIL_MAXLENGTH) {
+      const businessCase = new BusinessCase('user.email.maxlength', 'E-mail must have a maximum of 250 characters');
+      return Promise.resolve(businessCase);
+    }
+    return Promise.resolve();
+  }
+
+  emailMustBeUnique({email, id}) {
+    const quering = {
+      attributes: ['id'],
+      where: {email}
+    };
+
+    return this.userModel.findOne(quering)
+      .then(result => {
+        if (result && result.id !== id) {
+          const businessCase = new BusinessCase('user.email.unique', 'E-mail must be unique');
+          return Promise.resolve(businessCase);
+        }
+        return Promise.resolve();
+      });
+  }
+
+  nameIsRequired({name}) {
+    if (!trim(name)) {
+      const businessCase = new BusinessCase('user.name.required', 'Name is required');
+      return Promise.resolve(businessCase);
+    }
+    return Promise.resolve();
+  }
+
+  nameMustHaveMaximum100Characters({name}) {
+    if (name && name.length > NAME_MAXLENGTH) {
+      const businessCase = new BusinessCase('user.name.maxlength', 'Name must have a maximum of 100 characters');
+      return Promise.resolve(businessCase);
+    }
+    return Promise.resolve();
+  }
+
+  passwordIsRequired({password}) {
+    if (!trim(password)) {
+      const businessCase = new BusinessCase('user.password.required', 'Password is required');
+      return Promise.resolve(businessCase);
+    }
+    return Promise.resolve();
+  }
+
+  passwordMustHaveMinimum5Characters({password}) {
+    if (password && password.length < PASSWORD_MINLENGTH) {
+      const businessCase = new BusinessCase('user.password.minlength', 'Password must have a minimum  of 5 characters');
+      return Promise.resolve(businessCase);
+    }
+    return Promise.resolve();
+  }
+}
+
+module.exports = UserValidate;

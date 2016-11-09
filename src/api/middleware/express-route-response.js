6d7182f9c13544bf03313339dc7115386cde917d
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-09 15:15:05 -0200

    update envs in express-route-response

--- src/api/middleware/express-route-response.js
+++ src/api/middleware/express-route-response.js
@@ -1,9 +1,10 @@
 'use strict';
 const compression = require('compression');
+const environment = require('../environment');
 const express = require('express');
 
 module.exports = app => {
-  const configuration = app.configuration;
+  const configuration = environment.server;
 
   app.set('json spaces', configuration.server.jsonSpaces);
   app.use(express.static(configuration.server.webDir));

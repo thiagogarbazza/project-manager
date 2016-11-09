481d21cea9cd5debf77c86ac462463e86206b91b
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-09 13:13:57 -0200

    create a client routes

---
+++ src/api/domain/client/client-route.js
@@ -0,0 +1,31 @@
+'use strict';
+const CLientService = require('./client-service');
+const routeResolver = require('express-route-resolver');
+
+module.exports = app => {
+  const clientService = new CLientService(app);
+
+  app.route('/service/client')
+    .get((req, res) => {
+      const promise = clientService.find(req.query);
+      routeResolver.onFind(res, promise);
+    })
+    .post((req, res) => {
+      const promise = clientService.create(req.body, req.user);
+      routeResolver.onCreate(res, promise);
+    });
+
+  app.route('/service/client/:uuid')
+    .get((req, res) => {
+      const promise = clientService.findById(req.params.uuid);
+      routeResolver.onFindOne(res, promise);
+    })
+    .put((req, res) => {
+      const promise = clientService.update(req.params.uuid, req.body, req.user);
+      routeResolver.onUpdate(res, promise);
+    })
+    .delete((req, res) => {
+      const promise = clientService.destroy(req.params.uuid);
+      routeResolver.onDelete(res, promise);
+    });
+};

46fb2c482434929b8c0c993145b71f3372b9df4a
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-09 15:18:30 -0200

    update auth

--- src/api/configuration/authentication.js
+++ src/api/configuration/authentication.js
@@ -1,26 +1,24 @@
 'use strict';
-const configuration = require('../configuration');
+const {ExtractJwt, Strategy} = require('passport-jwt');
+const environment = require('../environment');
 const passport = require('passport');
-const Strategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const UserService = require('../domain/security/user/user-service');
 
 module.exports = app => {
-  const Users = app.domain.user.Users;
+  const userService = new UserService(app);
+  const configuration = environment.authentication;
 
   const params = {
     jwtFromRequest: ExtractJwt.fromAuthHeader(),
-    secretOrKey: configuration.authentication.passphrase
+    secretOrKey: configuration.passphrase
   };
 
-  const strategy = new Strategy(params, (payload, done) => {
-    Users.findById(payload.id)
+  const strategy = new Strategy(params, ({id}, done) => {
+    userService.findById(id)
       .then(user => {
         if (user) {
-          return done(null, {
-            id: user.id
-          });
+          return done(null, {id});
         }
-
         return done(null, false);
       })
       .catch(error => done(error, null));
@@ -29,7 +27,7 @@ module.exports = app => {
   passport.use(strategy);
 
   app.authentication = {
-    authenticate: () => passport.authenticate('jwt', configuration.authentication.options),
+    authenticate: () => passport.authenticate('jwt', configuration.options),
     initialize: () => passport.initialize()
   };
 };

a4797eb75f8450a7518ed64ca13b6bdc5572ef27
Thiago Garbazza <thiagogarbazza@gmail.com>
2016-11-15 18:31:13 -0200

    create file task run api-test-e2e

---
+++ build-tasks/api-test-e2e.js
@@ -0,0 +1,31 @@
+'use strict';
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const env = require('gulp-env');
+const CI = process.env.CI === 'true';
+
+const envs = env.set({
+  NODE_ENV: 'test'
+});
+
+const OPTIONS = {
+  reporter: CI ? 'xunit' : null,
+  reporterOptions: {
+    output: CI ? 'shippable/testresults/api-test-e2e-result.xml' : null
+  },
+  require: [
+    './src/api-test-e2e/helpers',
+    './src/api-test-e2e/database',
+    './src/api-test-e2e/result-file'
+  ],
+  timeout: 30000
+};
+
+gulp.task('api-test-e2e', done => gulp
+  .src(['src/api-test-e2e/**/*spec.js'], {read: false})
+  .pipe(envs)
+  .pipe(mocha(OPTIONS))
+  .once('end', () => {
+    process.exit();
+  })
+);

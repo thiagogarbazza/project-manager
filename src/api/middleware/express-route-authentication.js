581cb41b3911917acfe0db209157936dd23540a3
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-16 16:47:42 -0200

    fix checkstyle for improve reading

--- src/api/middleware/express-route-authentication.js
+++ src/api/middleware/express-route-authentication.js
@@ -4,11 +4,14 @@ const validator = require('validator');
 module.exports = app => {
   app.use(app.authentication.initialize());
 
-  app.route('/service/**').all(function scapeTokenRoute(req, res, next) {
-    if (validator.matches(req.url, '^/service/security/token$', 'ig')) {
-      next('route');
-    } else {
-      next();
-    }
-  }, app.authentication.authenticate());
+  app.route('/service/**')
+    .all(scapeTokenRoute, app.authentication.authenticate());
 };
+
+function scapeTokenRoute(req, res, next) {
+  if (validator.matches(req.url, '^/service/security/token$', 'ig')) {
+    next('route');
+  } else {
+    next();
+  }
+}

b53ab97a643bf709814ea27f97af55d654d5a4cc
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-16 11:17:05 -0200

    create a document model

---
+++ src/api/domain/document/document-model.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = (sequelize, DataType) => {
+  const definition = {
+    content: {
+      allowNull: true,
+      type: DataType.TEXT
+    },
+    createdAt: {
+      allowNull: false,
+      field: 'created_at',
+      type: DataType.DATE
+    },
+    id: {
+      allowNull: false,
+      defaultValue: DataType.UUIDV4,
+      primaryKey: true,
+      type: DataType.UUID,
+      unique: true
+    },
+    name: {
+      allowNull: false,
+      type: DataType.STRING(100)
+    },
+    updatedAt: {
+      allowNull: false,
+      field: 'updated_at',
+      type: DataType.DATE
+    }
+  };
+
+  const options = {
+    classMethods: {
+      associate: domain => {
+        const documentModel = domain.document.DocumentModel;
+        const projectModel = domain.project.ProjectModel;
+        const userModel = domain.security.user.UserModel;
+
+        documentModel.belongsTo(projectModel, {
+          as: 'project',
+          foreignKey: {
+            field: 'project_id',
+            name: 'projectId'
+          }
+        });
+
+        documentModel.belongsTo(userModel, {
+          as: 'creationByUser',
+          foreignKey: {
+            field: 'created_by',
+            name: 'createdBy'
+          }
+        });
+
+        documentModel.belongsTo(userModel, {
+          as: 'updatedByUser',
+          foreignKey: {
+            field: 'updated_by',
+            name: 'updatedBy'
+          }
+        });
+      }
+    },
+    schema: 'document',
+    tableName: 'tbl_document'
+  };
+
+  return sequelize.define('DocumentModel', definition, options);
+};

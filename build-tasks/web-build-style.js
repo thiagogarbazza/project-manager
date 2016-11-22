4ea372d493b35c4291fe93431727fa907ea21b7d
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-22 11:21:53 -0200

    add old template in styl

--- build-tasks/web-build-style.js
+++ build-tasks/web-build-style.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 
 gulp.task('web-build-style', function() {
-  return gulp.src('src/web/styles/main.styl')
+  return gulp.src(['src/web/styles/main.styl', 'src/web/styles/template.styl'])
     .pipe(stylus())
     .pipe(gulp.dest('dist/web/styles/'));
 });

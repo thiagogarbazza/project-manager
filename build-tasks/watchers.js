5f9ff4d158411ded4a4105ffb296e661b1e312cf
thiagogarbazza <thiagogarbazza@gmail.com>
2016-11-23 16:27:08 -0200

    create whatchers

---
+++ build-tasks/watchers.js
@@ -0,0 +1,6 @@
+'use strict';
+const gulp = require('gulp');
+
+gulp.task('w-styles', () => gulp.watch('src/web/styles/**/*.styl', ['web-build-style']));
+gulp.task('w-api-unit', () => gulp.watch('src/api*/**/*.js', ['api-test-unit']));
+gulp.task('w-api-e2e', () => gulp.watch('src/api*/**/*.js', ['api-test-e2e']));

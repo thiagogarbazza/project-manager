56d5ab8951820878c76770c8e8b7ccbed2b21868
thiagogarbazza <thiagogarbazza@gmail.com>
2016-12-05 13:19:48 -0200

    remove require not use

--- src/api/configuration/sequelize.js
+++ src/api/configuration/sequelize.js
@@ -1,7 +1,5 @@
 'use strict';
 const environment = require('../environment');
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
 const winston = require('winston');
 
--- src/api/configuration/winston.js
+++ src/api/configuration/winston.js
@@ -2,7 +2,7 @@
 const environment = require('../environment');
 const winston = require('winston');
 
-module.exports = app => {
+module.exports = () => {
   const configuration = environment.logger;
   winston.level = configuration.level;
   winston.colorize = configuration.colorize;

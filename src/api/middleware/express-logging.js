85f6957bac40916330d2916bbb76448bce524d1a
thiagogarbazza <thiagogarbazza@gmail.com>
2016-12-23 13:46:41 -0200

    [api] fix logging express

--- src/api/middleware/express-logging.js
+++ src/api/middleware/express-logging.js
@@ -1,10 +1,22 @@
 'use strict';
 
+const HttpStatus = require('http-status-codes');
 const morgan = require('morgan');
 const winston = require('winston');
 
-module.exports = app => {
-  const OPTIONS = {stream: {write: message => winston.info(message)}};
+const OPTIONS = {
+  skip,
+  stream: {write: loggging}
+};
 
+module.exports = app => {
   app.use(morgan('common', OPTIONS));
 };
+
+function loggging(message) {
+  return winston.warn(message);
+}
+
+function skip(req, res) {
+  return res.statusCode < HttpStatus.BAD_REQUEST;
+}

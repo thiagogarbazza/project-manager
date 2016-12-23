7231b1b59dbd8c4e8f99fc7804bee607991960c5
thiagogarbazza <thiagogarbazza@gmail.com>
2016-12-23 14:58:08 -0200

    fix coverage

--- build-tasks/api-test-unit.js
+++ build-tasks/api-test-unit.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const gulp = require('gulp');
-const istanbul = require('gulp-istanbul');
 const mocha = require('gulp-mocha');
 
 const CI = process.env.CI === 'true';
@@ -10,22 +9,8 @@ const OPTIONS_MOCHA = {
   reporterOptions: {output: CI ? 'shippable/testresults/api-test-unit-result.xml' : null},
   require: ['./src/api-test/helpers']
 };
-const OPTIONS_ISTANBUL = {
-  dir: './shippable/codecoverage/',
-  reportOpts: {cobertura: {dir: './shippable/codecoverage/', file: 'api-test-unit-cobertura.xml'}},
-  reporters: ['cobertura']
 
-};
-
-gulp.task('api-test-unit-istanbul', () => gulp
-  .src(['src/api/**/*.js'])
-  .pipe(istanbul())
-  .pipe(istanbul.hookRequire())
-);
-
-gulp.task('api-test-unit', ['api-test-unit-istanbul'], () => gulp
+gulp.task('api-test-unit', () => gulp
   .src(['src/api-test/**/*spec.js'], {read: false})
   .pipe(mocha(OPTIONS_MOCHA))
-  .pipe(istanbul.writeReports(OPTIONS_ISTANBUL))
-
 );
---
+++ build-tasks/coverage.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require('gulp');
+const istanbul = require('gulp-istanbul');
+
+const CI = process.env.CI === 'true';
+const OPTIONS_ISTANBUL = {
+  dir: './shippable/codecoverage/',
+  reportOpts: {cobertura: {dir: './shippable/codecoverage/', file: 'api-test-unit-cobertura.xml'}},
+  reporters: CI ? ['cobertura'] : ['text']
+};
+
+gulp.task('coverage-before', () => gulp
+  .src(['src/api/**/*.js'])
+  .pipe(istanbul())
+  .pipe(istanbul.hookRequire())
+);
+
+gulp.task('coverage-after', () => gulp
+  .src(['src/api-test/**/*spec.js'], {read: false})
+  .pipe(istanbul.writeReports(OPTIONS_ISTANBUL))
+);
+
+const runSequence = require('run-sequence');
+gulp.task('MT', cb => runSequence('coverage-before', 'api-test-unit', 'coverage-after', cb));

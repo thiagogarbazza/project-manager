f804bb5d10ee17a807a0b3baf95328f6ab856c4d
thiagogarbazza <thiagogarbazza@gmail.com>
2017-01-05 09:41:47 -0200

    [code-style] fix helpers

--- src/api-test/helpers.js
+++ src/api-test/helpers.js
@@ -17,6 +17,26 @@ global.mockery = mockery;
 global.simpleMock = simpleMock;
 
 global.properties = {
-  BIG_TEXT: 'dASDFASDFASDFASDFasdfASDFASDFajksdhfjhasdfASDFasdnbfjasdufasDFASDfjkasuidhfuiashrbfasdfASDFAsdkfjhasjdhfjkasdFASDFasdjkfjashdjfaSDFASDFdASDFASDFASDFASDFasdfASDFASDFajksdhfjhasdfASDFasdnbfjasdufasDFASDfjkasuidhfuiashrbfasdfASDFAsdkfjhasjdhfjkasdFASDFasdjkfjashdjfaSDFASDFdASDFASDFASDFASDFasdfASDFASDFajksdhfjhasdfASDFasdnbfjasdufasDFASDfjkasuidhfuiashrbfasdfASDFAsdkfjhasjdhfjkasdFASDFasdjkfjashdjfaSDFASDFdASDFASDFASDFASDFasdfASDFASDFajksdhfjhasdfASDFasdnbfjasdufasDFASDfjkasuidhfuiashrbfasdfASDFAsdkfjhasjdhfjkasdFASDFasdjkfjashdjfaSDFASDFdASDFASDFASDFASDFasdfASDFASDFajksdhfjhasdfASDFasdnbfjasdufasDFASDfjkasuidhfuiashrbfasdfASDFAsdkfjhasjdhfjkasdFASDFasdjkfjashdjfaSDFASDF',
+  BIG_TEXT: `
+  Lorem ipsum dolor sit amet, aperiri persecuti te vim, mei dicta eripuit cu. Id ius debet repudiandae interpretaris.
+  Noluisse scripserit te cum. Te euismod habemus pri.
+
+  Eu est bonorum maiestatis, an vix amet lorem rationibus.
+  Nam at conceptam repudiandae definitionem, sea an porro singulis incorrupte.
+  Sit case modus conceptam cu, cu audire expetendis complectitur pro, dicta option consequat eos ei.
+  Sed ad fastidii tractatos, nam eu tempor perfecto quaestio. Pri ponderum praesent intellegat an, vim ea exerci verear nusquam.
+
+  Sint liber dictas at vix, ei his postulant democritum, porro eripuit mei in.
+  Has ut facilisi vituperata ullamcorper, vim brute velit vivendo ei. Has in habeo nullam, sed torquatos philosophia et.
+  Sit et labores corrumpit signiferumque, id invidunt sensibus sit.
+
+  Saepe vivendum has te, dicta nusquam has te.
+  Id causae timeam eos. Minim integre no duo, soleat dissentiet mei no. Laoreet inermis delicatissimi ne sea, vix id labore referrentur.
+  Nisl prima mucius at vim, cum cu virtute aliquid tractatos.
+
+  Ex aeque aliquando cotidieque vix, inimicus percipitur definitiones pri ei, ea eos aeque reprimique scribentur.
+  Per ut tota lorem interesset, ius iriure indoctum eu.
+  Cum eirmod accusam adolescens an, no verterem salutandi eum, nam ne tota etiam. Salutatus consectetuer eos ex, qui ea eirmod maiorum intellegam.
+  `,
   INVALID_COLOR: 'invalid-color'
 };

d5e105d0e15f18546b5fae44a4ff5dce8ac71b07
thiagogarbazza <thiagogarbazza@gmail.com>
2017-03-02 15:45:03 -0300

    [web] fix task build styles

--- build-tasks/web-build-style.js
+++ build-tasks/web-build-style.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 
 gulp.task('web-build-style', () => gulp
-  .src(['src/web/styles/main.styl', 'src/web/styles/beyond.min.styl'])
+  .src(['src/web/styles/main.styl'])
   .pipe(stylus())
-  .pipe(gulp.dest('dist/web/ba-new/assets/css'))
+  .pipe(gulp.dest('dist/web/styles/'))
 );

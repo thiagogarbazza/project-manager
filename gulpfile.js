48b2abf996f29a48c4a733f3f965925f11e30120
thiagogarbazza <thiagogarbazza@gmail.com>
2017-03-30 13:52:00 -0300

    [code-style] fix eslint-rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,10 @@
 'use strict';
+
 const gulp = require('gulp');
 const runSequence = require('run-sequence');
-const tasks = require('require-dir')('./build-tasks');
 
-// const eslint = require('gulp-eslint');
-// const htmlhint = require('gulp-htmlhint');
+require('require-dir')('./build-tasks');
 
-// gulp.task('eslint', () => gulp.src(['**/*.js', '!node_modules/**', '!web/libs/**'])
-//   .pipe(eslint())
-//   .pipe(eslint.format())
-//   .pipe(eslint.failAfterError())
-// );
-//
-// gulp.task('htmlhint', () => gulp.src(['**/*.html', '!node_modules/**', '!web/libs/**'])
-//   .pipe(htmlhint())
-//   .pipe(htmlhint.reporter('htmlhint-stylish'))
-//   .pipe(htmlhint.failReporter({ suppress: true }))
-// );
-//
-// gulp.task('qa', ['eslint', 'htmlhint']);
+gulp.task('verify', done => runSequence('coverage-before', 'api-test-unit', 'coverage-after', done));
 
-gulp.task('verify', cb => runSequence('coverage-before', 'api-test-unit', 'coverage-after', cb));
-
-gulp.task('make', cb => runSequence('clean', 'build-css', cb));
+gulp.task('make', done => runSequence('clean', 'build-css', done));
